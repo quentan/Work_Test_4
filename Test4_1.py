5008b68027bd5e50fc3b23c0fbe9804d9ed39813
Quentan <quentan@gmail.com>
2016-01-05 11:25:43 +0000

    test medical image reading

--- Test4_1.py
+++ Test4_1.py
@@ -8,8 +8,10 @@ import sys
 import vtk
 
 from PyQt4 import QtGui
+from PyQt4 import QtCore
 
 from lib import ui_main_window
+from lib import read_image
 
 
 class Basic(QtGui.QMainWindow):
@@ -43,8 +45,32 @@ class Basic(QtGui.QMainWindow):
         self.iren = self.ren_win.GetInteractor()
         self.iren.Initialize()
 
+        self.reader = read_image.read_image()
+
+        self.ui.open_dicom_btn.clicked.connect(self.on_open_dicom_folder)
+        self.ui.open_meta_btn.clicked.connect(self.on_open_metaimage)
+
         self.move(100, 100)
         self.show()
+
+    def on_open_dicom_folder(self):
+
+        folder_name = QtGui.QFileDialog.getExistingDirectory(
+            self, 'Open DICOM Folder', QtCore.QDir.currentPath(),
+            QtGui.QFileDialog.ShowDirsOnly)
+        folder_name = str(folder_name)  # QString --> Python String
+
+        self.reader = read_image.read_dicom(folder_name)
+
+    def on_open_metaimage(self):
+
+        fname = QtGui.QFileDialog.getOpenFileName(
+            self, '&Open Meta Image', QtCore.QDir.currentPath(),
+            'Meta Image (*.mha, *.mhd')
+
+        f = open(fname, 'r')
+
+        self.reader = read_image.read_metaimage(f)
 
 
 if __name__ == '__main__':
--- lib/ui_main_window.py
+++ lib/ui_main_window.py
@@ -8,6 +8,24 @@ class Ui_MainWindow(object):
 
     def setup_ui(self, main_window):
 
+        exitAction = QtGui.QAction(
+            QtGui.QIcon('exit.png'), 'E&xit', main_window)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit Application')
+        exitAction.triggered.connect(main_window.close)
+
+        # Menu
+        self.menubar = QtGui.QMenuBar(main_window)
+        self.menubar.setNativeMenuBar(False)
+        self.file_menu = self.menubar.addMenu('&File')
+        self.file_menu.addAction(exitAction)
+        main_window.setMenuBar(self.menubar)
+
+        # Status Bar
+        self.statusbar = QtGui.QStatusBar(main_window)
+        self.statusbar.showMessage('Ready')
+        main_window.setStatusBar(self.statusbar)
+
         main_window.setObjectName('Main Window')
         main_window.resize(640, 480)
 
@@ -16,8 +34,14 @@ class Ui_MainWindow(object):
 
         # Widgets
         self.vtk_widget = QVTKRenderWindowInteractor(self.central_widget)
+        self.vtk_widget.setStatusTip('VTK Rendering Pane')
+
+        self.open_dicom_btn = QtGui.QPushButton('&Open DICOM folder')
+        self.open_meta_btn = QtGui.QPushButton('Open Meta Image')
 
         # Layout of widgets
         self.gridlayout.addWidget(self.vtk_widget, 2, 0, 10, 1)
+        self.gridlayout.addWidget(self.open_dicom_btn, 2, 1)
+        self.gridlayout.addWidget(self.open_meta_btn, 3, 1)
 
         main_window.setCentralWidget(self.central_widget)
